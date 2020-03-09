162b80335d278793dfe8309d9c45abab77f42acf
f-w <fred.wen@gov.bc.ca>
2020-03-09 15:35:23 -0700

    confirmed redshift connection using pg

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # foi-report-download
+
+## Configs
+| Environment Variable | Mandatory | Description |
+|----------------------|-----------|-----------------|
+| PGUSER | Yes |  |
+| PGHOST | Yes |  |
+| PGPASSWORD | No |  |
+| PGDATABASE | Yes |  |
+| PGPORT | No | Default to 5439 |
---
+++ index.js
@@ -0,0 +1,10 @@
+;(async function() {
+  const { Client } = require('pg')
+  const client = new Client({
+    port: process.env.PGPORT || 5439
+  })
+  await client.connect()
+  const res = await client.query('SELECT $1::text as message', ['Hello world!'])
+  console.log(res.rows[0].message) // Hello world!
+  await client.end()
+})()
