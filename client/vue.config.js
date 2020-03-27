93d576824d479e868ae429d0b0ce513f774ad3c5
f-w <fred.wen@gov.bc.ca>
2020-03-27 16:29:22 -0700

    set snowplow url in env

--- client/vue.config.js
+++ client/vue.config.js
@@ -7,5 +7,19 @@ module.exports = {
         target: 'http://localhost:8080'
       }
     }
+  },
+  chainWebpack: config => {
+    config.module
+      .rule('html')
+      .test(/\.html$/)
+      .use('html-loader')
+      .loader('html-loader')
+      .tap(options => {
+        // modify the options...
+        return {
+          minimize: false
+        }
+      })
+      .end()
   }
 }
