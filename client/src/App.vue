8be57a3775a6796c69148cb3ef9dcdf0acaa627d
f-w <fred.wen@gov.bc.ca>
2020-03-31 16:46:04 -0700

    added static text

--- client/src/App.vue
+++ client/src/App.vue
@@ -14,6 +14,99 @@
       <v-toolbar-title>FOI Report Download</v-toolbar-title>
     </v-app-bar>
     <v-content>
+      <v-container>
+        <v-row>
+          <v-col cols="12">
+            <h2>
+              Welcome to the Information Access Operations one stop shop for
+              reporting on FOI requests.
+            </h2>
+            <p>
+              We’ve heard your collective feedback and have created this tool to
+              allow you to run file reports on an as and when needed basis, so
+              you no longer need to rely on Monday’s Executive Weekly Reports
+              for your mid-/late-week meetings. As an additional feature, and
+              also based on your feedback, we have included an option to have
+              the report produced in either Excel or PDF format.
+            </p>
+            <h2>Instructions for use:</h2>
+            <p>
+            <ol>
+              <li>Select your organization from the drop down menu.</li>
+              <li>
+                Select the file status you would like to report on or leave it
+                at the default All Open setting.
+              </li>
+              <li>
+                Select the applicant types you would like to report on or leave
+                it at the default All Applicants setting.
+              </li>
+              <li>
+                Enter the applicable start or due date ranges.
+              </li>
+              <li>
+                Select your preferred file format.
+              </li>
+              <li>
+                Click submit to run the report.
+              </li>
+            </ol>
+            </p>
+            <h2>FOI Performance and other statistics:</h2>
+            <p>
+            <ul>
+              <li>
+                IAO’s
+                <a
+                  href="https://sharedservices.gov.bc.ca/IAO_2.0/RSU/Dashboards/Forms/AllItems.aspx"
+                  >Dashboards</a
+                >
+                will take you to three Excel spreadsheets that have pivot tables
+                to aid in your filtering and already filtered sheets that
+                summarize FOI data. Just click on your ministry to filter out
+                the rest.
+              </li>
+              <li>
+                FOI related information in the
+                <a
+                  href="https://catalogue.data.gov.bc.ca/dataset/freedom-of-information-foi-statistics"
+                  >DataBC Data Catalogue</a
+                >
+                is updated quarterly.
+              </li>
+              <li>
+                New FOI response packages that are suitable for publication are
+                published on the
+                <a
+                  href="https://www2.gov.bc.ca/gov/content/governments/about-the-bc-government/open-government/open-information"
+                  >Open Information</a
+                >
+                website daily and a number of other proactive disclosure
+                products are also published on regularly recurring schedule.
+              </li>
+              <li>
+                <a
+                  href="https://www2.gov.bc.ca/gov/content/governments/about-the-bc-government/open-government/open-information/foi-request-statistics"
+                  >Annual Reports</a
+                >
+                on the Administration of FOIPPA are published on the IAO
+                website.
+              </li>
+            </ul>
+            </p>
+            <h2>IAO Contacts</h2>
+            <p>
+            Your
+            <a
+              href="https://www2.gov.bc.ca/gov/content/governments/about-the-bc-government/open-government/open-information/freedom-of-information/managers-information-access"
+              >IAO Manager</a
+            >
+            is available to discuss ad hoc reporting requests and any other
+            issues relating to FOI requests.
+            </p>
+          </v-col>
+        </v-row>
+      </v-container>
       <Form />
     </v-content>
     <v-footer color="#036" padless>
