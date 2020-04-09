3e769e06e53e485d904de9987eca89e0e5d0f5f1
f-w <fred.wen@gov.bc.ca>
2020-04-09 11:48:20 -0700

    re-arranged table column

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ const orgMap = {
   PSA: 'Public Service Agency',
   PSS: 'Ministry of Public Safety and Solicitor General',
   TAC: 'Ministry of Tourism, Arts and Culture',
-  TRA: 'Ministry of Transportation and Infrastructure'
+  TRA: 'Ministry of Transportation and Infrastructure',
 }
 
 const commonOrgGroup = {
@@ -58,7 +58,7 @@ const commonOrgGroup = {
   PSS: ['PSS', 'EMB'],
   MAG: ['MAG', 'JAG'],
   JTT: ['JTT', 'JTI', 'MIT', 'MSB'],
-  FIN: ['FIN', 'PSA']
+  FIN: ['FIN', 'PSA'],
 }
 
 const orgGroupByDate = [
@@ -70,8 +70,8 @@ const orgGroupByDate = [
       FIN: [...commonOrgGroup.FIN, 'GCP'],
       EMP: [...commonOrgGroup.EMP, 'HOU'],
       JTT: [...commonOrgGroup.JTT, 'LDB'],
-      MAG: [...commonOrgGroup.MAG, 'OCC']
-    }
+      MAG: [...commonOrgGroup.MAG, 'OCC'],
+    },
   },
   {
     date: moment('2015-07-01'),
@@ -80,9 +80,9 @@ const orgGroupByDate = [
       FIN: [...commonOrgGroup.FIN, 'BRD'],
       CTZ: [...commonOrgGroup.CTZ, 'GCP'],
       MAH: [...commonOrgGroup.MAH, 'HOU'],
-      PSS: [...commonOrgGroup.PSS, 'LDB', 'OCC']
-    }
-  }
+      PSS: [...commonOrgGroup.PSS, 'LDB', 'OCC'],
+    },
+  },
 ]
 const statusMap = {
   'All Open': [
@@ -97,12 +97,12 @@ const statusMap = {
     'On Hold-Other',
     'Perfected',
     'Received',
-    'Request for Docs Sent'
+    'Request for Docs Sent',
   ],
   'All On-Hold': [
     'On Hold-Fee Related',
     'On Hold-Need Info/Clarification',
-    'On Hold-Other'
+    'On Hold-Other',
   ],
   'All Open excluding on-hold': [
     'Amended',
@@ -113,9 +113,9 @@ const statusMap = {
     'Documents Delivered',
     'Perfected',
     'Received',
-    'Request for Docs Sent'
+    'Request for Docs Sent',
   ],
-  'All Closed': ['Closed']
+  'All Closed': ['Closed'],
 }
 
 const summaryStatusMap = {
@@ -128,13 +128,13 @@ const summaryStatusMap = {
     'Documents Delivered',
     'Perfected',
     'Received',
-    'Request for Docs Sent'
+    'Request for Docs Sent',
   ],
   onHold: [
     'On Hold-Fee Related',
     'On Hold-Need Info/Clarification',
-    'On Hold-Other'
-  ]
+    'On Hold-Other',
+  ],
 }
 
 const generalSummaryApplicantTypes = [
@@ -147,7 +147,7 @@ const generalSummaryApplicantTypes = [
   'Other Governments',
   'Other Public Body',
   'Political Party',
-  'Researcher'
+  'Researcher',
 ]
 if (process.env.FILE_STORE_PATH) {
   storeOptions.path = process.env.FILE_STORE_PATH
@@ -163,7 +163,7 @@ app.use(
     secret: 'secret',
     resave: false,
     saveUninitialized: true,
-    store: store
+    store: store,
   })
 )
 app.use(keycloak.middleware())
@@ -172,8 +172,8 @@ const { Pool } = require('pg')
 const pool = new Pool({
   port: process.env.PGPORT || 5439,
   ssl: {
-    rejectUnauthorized: false
-  }
+    rejectUnauthorized: false,
+  },
 })
 const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -183,8 +183,8 @@ const pdfFonts = {
     normal: 'fonts/Roboto-Regular.ttf',
     bold: 'fonts/Roboto-Medium.ttf',
     italics: 'fonts/Roboto-Italic.ttf',
-    bolditalics: 'fonts/Roboto-MediumItalic.ttf'
-  }
+    bolditalics: 'fonts/Roboto-MediumItalic.ttf',
+  },
 }
 
 app.get('/ping', async (req, res) => {
@@ -202,8 +202,9 @@ app.post('/FOI-report', async (req, res) => {
   }
 
   const selectStmt =
-    'select request_id, start_date, duedate, status, applicant_type, ' +
-    'analyst, description, current_activity from foi.foi'
+    'select request_id, applicant_type, description, start_date, duedate, ' +
+    'current_activity, analyst, no_pages_in_request::integer ' +
+    'from foi.foi'
   const summarySelectStmt =
     'select type, applicant_type, status, case when ' +
     'duedate < trunc(getdate()) then true else false end ' +
@@ -233,7 +234,7 @@ app.post('/FOI-report', async (req, res) => {
         i === 0 &&
           includeAllWhereClauses &&
           filterMessages.push(
-            `organization in (${orgCodes.map(e => orgMap[e]).join(', ')})`
+            `organization in (${orgCodes.map((e) => orgMap[e]).join(', ')})`
           )
       }
       if (req.body.startDateFrom) {
@@ -314,17 +315,21 @@ app.post('/FOI-report', async (req, res) => {
         // Create a reusable style
         const headerStyle = wb.createStyle({
           font: {
-            bold: true
-          }
+            bold: true,
+          },
         })
         const dataRowStyle = wb.createStyle({
-          alignment: { wrapText: true, vertical: 'top' }
+          alignment: { wrapText: true, vertical: 'top' },
         })
 
         ws.column(1).setWidth(18)
-        ws.column(4).setWidth(16)
-        ws.column(5).setWidth(16)
-        ws.column(6).setWidth(50)
+        ws.column(2).setWidth(16)
+        ws.column(3).setWidth(50)
+        ws.column(4).setWidth(14)
+        ws.column(5).setWidth(14)
+        ws.column(6).setWidth(18)
+        ws.column(7).setWidth(14)
+        ws.column(8).setWidth(18)
 
         let currRow = 1
         if (filterMessages.length > 0) {
@@ -337,37 +342,28 @@ app.post('/FOI-report', async (req, res) => {
         }
         if (
           rows.some(
-            e => e.duedate < moment().startOf('day') && e.status !== 'Closed'
+            (e) => e.duedate < moment().startOf('day') && e.status !== 'Closed'
           )
         ) {
           ws.cell(currRow++, 1).string(pastDueMsg)
         }
-        ws.cell(currRow, 1)
-          .string('request id')
-          .style(headerStyle)
-        ws.cell(currRow, 2)
-          .string('start date')
-          .style(headerStyle)
-          .style({ alignment: { horizontal: 'right' } })
-        ws.cell(currRow, 3)
-          .string('due date')
-          .style(headerStyle)
-          .style({ alignment: { horizontal: 'right' } })
+        ws.cell(currRow, 1).string('Request #').style(headerStyle)
+        ws.cell(currRow, 2).string('Applicant Type').style(headerStyle)
+        ws.cell(currRow, 3).string('Description').style(headerStyle)
         ws.cell(currRow, 4)
-          .string('status')
+          .string('Start Date')
           .style(headerStyle)
+          .style({ alignment: { horizontal: 'right' } })
         ws.cell(currRow, 5)
-          .string('applicant type')
+          .string('Due Date')
           .style(headerStyle)
-        ws.cell(currRow, 6)
-          .string('description')
-          .style(headerStyle)
-        ws.cell(currRow, 7)
-          .string('analyst')
-          .style(headerStyle)
+          .style({ alignment: { horizontal: 'right' } })
+        ws.cell(currRow, 6).string('Current Action').style(headerStyle)
+        ws.cell(currRow, 7).string('Analyst').style(headerStyle)
         ws.cell(currRow, 8)
-          .string('current activity')
+          .string('No Pages in Request')
           .style(headerStyle)
+          .style({ alignment: { horizontal: 'right' } })
         currRow++
 
         for (const [i, row] of rows.entries()) {
@@ -383,23 +379,23 @@ app.post('/FOI-report', async (req, res) => {
             .style(dataRowStyle)
             .style({ font: { color: color } })
           ws.cell(i + currRow, 2)
-            .date(row.start_date)
-            .style(dataRowStyle)
-            .style({ font: { color: color } })
-          ws.cell(i + currRow, 3)
-            .date(row.duedate)
-            .style(dataRowStyle)
-            .style({ font: { color: color } })
-          ws.cell(i + currRow, 4)
-            .string(row.status)
-            .style(dataRowStyle)
-            .style({ font: { color: color } })
-          ws.cell(i + currRow, 5)
             .string(row.applicant_type)
             .style(dataRowStyle)
             .style({ font: { color: color } })
-          ws.cell(i + currRow, 6)
+          ws.cell(i + currRow, 3)
             .string(row.description)
+            .style(dataRowStyle)
+            .style({ font: { color: color } })
+          ws.cell(i + currRow, 4)
+            .date(row.start_date)
+            .style(dataRowStyle)
+            .style({ font: { color: color } })
+          ws.cell(i + currRow, 5)
+            .date(row.duedate)
+            .style(dataRowStyle)
+            .style({ font: { color: color } })
+          ws.cell(i + currRow, 6)
+            .string(row.current_activity)
             .style(dataRowStyle)
             .style({ font: { color: color } })
           ws.cell(i + currRow, 7)
@@ -407,7 +403,7 @@ app.post('/FOI-report', async (req, res) => {
             .style(dataRowStyle)
             .style({ font: { color: color } })
           ws.cell(i + currRow, 8)
-            .string(row.current_activity)
+            .number(row.no_pages_in_request)
             .style(dataRowStyle)
             .style({ font: { color: color } })
         }
@@ -439,13 +435,13 @@ app.post('/FOI-report', async (req, res) => {
             }
             return acc
           },
-          generalSummaryApplicantTypes.map(e => ({
+          generalSummaryApplicantTypes.map((e) => ({
             applicantType: e,
             counts: {
               open: 0,
               onHold: 0,
-              openAndOverDue: 0
-            }
+              openAndOverDue: 0,
+            },
           }))
         )
         let generalSummaryTableBody = generalSummary.map((v, i) => {
@@ -454,7 +450,7 @@ app.post('/FOI-report', async (req, res) => {
             { text: v.applicantType },
             { text: v.counts.open, style: { alignment: 'right' } },
             { text: v.counts.onHold, style: { alignment: 'right' } },
-            { text: v.counts.openAndOverDue, style: { alignment: 'right' } }
+            { text: v.counts.openAndOverDue, style: { alignment: 'right' } },
           ]
         })
         generalSummaryTableBody.unshift([
@@ -462,14 +458,14 @@ app.post('/FOI-report', async (req, res) => {
           '',
           'Open',
           'On-Hold',
-          'Currently Open and Overdue'
+          'Currently Open and Overdue',
         ])
         generalSummaryTableBody.unshift([
           '',
           '',
           { text: 'General Requests', colSpan: 3, alignment: 'center' },
           '',
-          ''
+          '',
         ])
 
         const personalSummary = summaryRows.reduce(
@@ -490,7 +486,7 @@ app.post('/FOI-report', async (req, res) => {
           {
             open: 0,
             onHold: 0,
-            openAndOverDue: 0
+            openAndOverDue: 0,
           }
         )
         let personalSummaryTableBody = [
@@ -501,28 +497,28 @@ app.post('/FOI-report', async (req, res) => {
             { text: personalSummary.onHold, style: { alignment: 'right' } },
             {
               text: personalSummary.openAndOverDue,
-              style: { alignment: 'right' }
-            }
-          ]
+              style: { alignment: 'right' },
+            },
+          ],
         ]
         personalSummaryTableBody.unshift([
           '',
           '',
           'Open',
           'On-Hold',
-          'Currently Open and Overdue'
+          'Currently Open and Overdue',
         ])
         personalSummaryTableBody.unshift([
           '',
           '',
           { text: 'Personal Requests', colSpan: 3, alignment: 'center' },
           '',
-          ''
+          '',
         ])
 
         const printer = new PdfPrinter(pdfFonts)
         let hasOverdueOpenRows = false
-        const tableBody = rows.map(v => {
+        const tableBody = rows.map((v) => {
           let color = 'black'
           if (v.duedate < moment().startOf('day') && v.status !== 'Closed') {
             color = 'red'
@@ -533,30 +529,30 @@ app.post('/FOI-report', async (req, res) => {
           }
           return [
             { text: v.request_id, color: color },
-            { text: moment(v.start_date).format('YYYY-MM-DD'), color: color },
-            { text: moment(v.duedate).format('YYYY-MM-DD'), color: color },
-            { text: v.status, color: color },
             { text: v.applicant_type, color: color },
             { text: v.description, color: color },
+            { text: moment(v.start_date).format('YYYY-MM-DD'), color: color },
+            { text: moment(v.duedate).format('YYYY-MM-DD'), color: color },
+            { text: v.current_activity, color: color },
             { text: v.analyst, color: color },
-            { text: v.current_activity, color: color }
+            { text: v.no_pages_in_request, color: color, alignment: 'right' },
           ]
         })
         tableBody.unshift([
-          'request id',
-          'start date',
-          'due date',
-          'status',
-          'applicant type',
-          'description',
-          'analyst',
-          'current activity'
+          'Request #',
+          'Applicant Type',
+          'Description',
+          'Start Date',
+          'Due Date',
+          'Current Action',
+          'Analyst',
+          { text: 'No Pages in Request', alignment: 'right' },
         ])
         const dd = {
           content: [
             {
               image: 'citz.jpg',
-              width: 150
+              width: 150,
             },
             {
               columns: [
@@ -564,18 +560,18 @@ app.post('/FOI-report', async (req, res) => {
                   layout: 'lightHorizontalLines',
                   table: {
                     headerRows: 2,
-                    body: generalSummaryTableBody
-                  }
+                    body: generalSummaryTableBody,
+                  },
                 },
                 {
                   layout: 'lightHorizontalLines',
                   table: {
                     headerRows: 2,
-                    body: personalSummaryTableBody
-                  }
-                }
+                    body: personalSummaryTableBody,
+                  },
+                },
               ],
-              pageBreak: 'after'
+              pageBreak: 'after',
             },
             {
               columns: [
@@ -585,33 +581,33 @@ app.post('/FOI-report', async (req, res) => {
                     filterMessages.length === 0
                       ? []
                       : ['Report is filtered by', { ul: filterMessages }]
-                  )
+                  ),
                 },
                 {
                   stack: [
                     `Report generated on: ${moment().format('YYYY-MM-DD')}`,
-                    `Total records: ${rows.length}`
+                    `Total records: ${rows.length}`,
                   ],
                   width: '*',
-                  alignment: 'right'
-                }
-              ]
+                  alignment: 'right',
+                },
+              ],
             },
             {
               layout: 'lightHorizontalLines', // optional
               table: {
                 headerRows: 1,
-                widths: [40, 46, 46, 40, 60, '*', 30, 40],
-                body: tableBody
-              }
-            }
+                widths: [40, 60, '*', 46, 46, 40, 40, 40],
+                body: tableBody,
+              },
+            },
           ],
           defaultStyle: {
-            fontSize: 9
+            fontSize: 9,
           },
           pageOrientation: 'landscape',
           pageSize: 'LETTER',
-          pageMargins: 20
+          pageMargins: 20,
         }
         const options = {}
         const pdfDoc = printer.createPdfKitDocument(dd, options)
@@ -633,11 +629,11 @@ app.post('/FOI-report', async (req, res) => {
 app.set('view engine', 'ejs')
 app.set('views', 'client/dist')
 app.engine('html', require('ejs').renderFile)
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.render('index.html')
 })
 app.use(express.static('client/dist'))
-app.listen(port, function() {
+app.listen(port, function () {
   // don't timeout in 2min for node<13
   this.setTimeout(0)
   console.log(`launch http://localhost:${port} to explore`)
