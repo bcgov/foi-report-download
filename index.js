ee83326e124a74e4c10fe1b3f57647dcfad29764
BJenning <brendan.jennings@gov.bc.ca>
2022-05-05 10:20:38 -0700

    Fixed secretariat name.

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ const commonOrgGroup = {
   MAG: ['MAG', 'JAG'],
   JER: ['JER', 'JTI', 'MIT', 'MSB'],
   FIN: ['FIN', 'PSA'],
-  FIN: ['IRR', 'EAO']
+  FIN: ['IRR', 'DAS']
 }
 
 const orgGroupByDate = [
